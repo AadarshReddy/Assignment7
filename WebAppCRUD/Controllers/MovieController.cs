ce79f277338795e112394d0b18d79bab17d2c7c6
Aadarsh Reddy <areddy5757@gmail.com>
2024-02-07 15:30:08 +0530

    Add project files.

---
+++ WebAppCRUD/Controllers/MovieController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAppCRUD.Models;
+
+namespace WebAppCRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        static List<Movie> list = new List<Movie>()
+       {
+           new Movie{id=1,MName="Pokiri",StarCast="Mahesh Babu",Director="Puri",ReleaseDate= new DateTime(day:23,month:12,year:2012) },
+           new Movie{id=2,MName="Kaleja",StarCast="Mahesh Babu",Director="Trivikram",ReleaseDate= new DateTime(day:23,month:12,year:2024) }
+       };
+        [HttpGet]
+        public IEnumerable<Movie> Get()
+        {
+            return list;
+        }
+        [HttpGet("{id}")]
+        public ActionResult<Movie> Get(int id)
+        {
+            Movie mov = list.SingleOrDefault(x => x.id == id);
+            if (mov == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(mov);
+            }
+        }
+        [HttpDelete("{id}")]
+        public ActionResult<Movie> Delete(int id)
+        {
+            Movie mov = list.SingleOrDefault(x => x.id == id);
+            if (mov == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                list.Remove(mov);
+                return NoContent();
+            }
+        }
+        [HttpPost]
+        public ActionResult<Movie> Post(Movie newMov)
+        {
+            list.Add(newMov);
+            return CreatedAtAction(nameof(Get), newMov);
+        }
+        [HttpPut("{id}")]
+        public ActionResult<Movie> Put(int id, Movie UpMov)
+        {
+            Movie extMov = list.SingleOrDefault(x => x.id == id);
+            if (extMov == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+               
+
+                extMov.MName = UpMov.MName;
+                extMov.StarCast = UpMov.StarCast;
+                extMov.Director = UpMov.Director;
+                extMov.ReleaseDate = UpMov.ReleaseDate;
+
+                return NoContent();
+            }
+        }
+    }
+}
---
+++ WebAppCRUD/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAppCRUD.Models
+{
+    [Table("Movie")]
+    public class Movie
+    {
+        [Key]
+        public int id { get; set; }
+        public string? MName { get; set; }
+        public string? StarCast { get; set; }
+
+        public string? Director { get; set;}
+        public DateTime ReleaseDate { get; set; }
+
+    }
+}
